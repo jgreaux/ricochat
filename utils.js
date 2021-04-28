805243c49a6046f9f4832e35c5a034942a067cbc
jgreaux <greaux.jc@hotmail.fr>
2021-04-28 21:11:59 +0200

    Finish movment and adding sound

--- utils.js
+++ utils.js
@@ -338,37 +338,96 @@ const movePlayer = (pos, width) => {
 }
 
 const move = (dir) =>{
-    const currentPos = players[currentPlayer.id].pos;
-    movePlayer(computeDestination(currentPos, dir, mapWidth),mapWidth);
+    const player = players[currentPlayer.id];
+    movePlayer(computeDestination(player.pos, dir, mapWidth, player.color),mapWidth);
+    catSound.play();
 }
 
-const computeDestination = (pos, direction, width) => {
-    let step = null;
-    let destination = null;
+const computeDestination = (pos, direction, width, color) => {
+    let step = getDir(direction, width);
     let currentPos = pos; 
-     switch (direction) {
-         case 'right':
-            step = 1;
-            break;
-        case 'left':
-            step = -1;
-            break;
-        case 'up':
-            step = - width;
-            break;
-        case 'down':
-            step = width;
-            break;
-     }
-    while (destination === null) {
-        currentPos += step;
-        if (currentMap[currentPos] != 0) {
-            destination = currentPos;
+    
+     let currentStep = step;
+     let destination = null;
+     while (destination === null) {
+        const listPlayersPosition = Object.keys(players).map((key)=> players[key].pos);
+        if (listPlayersPosition.includes(currentPos + currentStep)){
+           destination = currentPos;
+           break;
         }
-    }
-    return currentPos;
+
+         const currentCase = currentMap[currentPos];
+         const nextCase = currentMap[currentPos + currentStep];
+
+         if(canIGo(currentCase, nextCase, currentStep, color)){
+            currentPos += currentStep
+         }else{
+             currentStep = yarnTurn(currentCase, currentStep, width);
+             if (currentStep !== 0) {
+                const keepPlayer = currentPlayer;
+                setTimeout(()=>{
+                    currentPlayer = keepPlayer;
+                    move(getDir(currentStep, width))
+                }, 1500);
+             }
+             destination = currentPos;
+         }        
+     }
+     return destination;
 }
 
+const getDir = (dir, width) =>{
+    const textValue = ['up', 'down', 'right', 'left'];
+    const numValue = [-width, width, 1, -1];
+    if(textValue.includes(dir)){
+        return numValue[textValue.indexOf(dir)];
+    }
+    if(numValue.includes(dir)){
+        return textValue[numValue.indexOf(dir)];
+    }
+}
+
+const canIGo = (currentCase, nextCase, direction, color) => {
+    const blueYarn = [9,10,11,12,21,22,23,24];
+    const greenYarn = [13,14,15,16,21,22,23,24];
+    const redYarn = [5,6,7,8,21,22,23,24];
+    const yellowYarn = [17,18,19,20,21,22,23,24];
+    const rightBorder = [1,-4,5,9,13,17,21,-2,6,10,14,18,22];
+    const leftBorder = [2,-1,7,11,15,19,23,-3,8,12,16,20,24];
+    const upBorder = [3,-4,5,9,13,17,21,-3,7,11,15,19,23];
+    const downBorder = [4,-2,6,10,14,18,22,-1,8,12,16,20,24];
+
+    let isBorder = false;
+    let sameColor = false;
+    isBorder = ((direction === 1 && (rightBorder.includes(currentCase) || leftBorder.includes(nextCase)))
+    || (direction > 1 && (downBorder.includes(currentCase) || upBorder.includes(nextCase)))
+    || (direction === -1 && (leftBorder.includes(currentCase) || rightBorder.includes(nextCase)))
+    || (direction < -1 && (upBorder.includes(currentCase) || downBorder.includes(nextCase))))
+
+    sameColor = (currentCase < 5
+    || (color === 'red' && redYarn.includes(currentCase)) 
+    || (color === 'blue' && blueYarn.includes(currentCase)) 
+    || (color === 'yellow' && yellowYarn.includes(currentCase)) 
+    || (color === 'green' && greenYarn.includes(currentCase))); 
+
+    return !isBorder || (isBorder && !sameColor);
+}
+
+const yarnTurn = (currentCase, step, width) => {
+    if (currentCase < 5) return 0;
+
+    const rightBorder = [1,-4,5,9,13,17,21,-2,6,10,14,18,22];
+    const upBorder = [3,-4,5,9,13,17,21,-3,7,11,15,19,23];
+
+    const borderIsRight = rightBorder.includes(currentCase);
+    const borderIsUp = upBorder.includes(currentCase);
+    const horizontalMove = Math.abs(step) === 1;
+
+    if (horizontalMove && borderIsUp) return width;
+    if (horizontalMove && !borderIsUp) return -width;
+    if (!horizontalMove && borderIsRight) return -1;
+    if (!horizontalMove && !borderIsRight) return 1;
+}
 //-----------------------------------------------------//
 
 const resetInputPlayer = () => {
@@ -437,3 +496,9 @@ const init = (map)=>{
     createPlayers();
     mapToDOM(map, playground);
 }
+
+//-----------------------------------------------------//
+const catSound = document.createElement('audio');
+catSound.innerHTML = `
+<source src="assets/cat.mp3" type="audio/mpeg">
+`
